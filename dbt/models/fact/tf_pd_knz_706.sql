b25d7a2214752f8b175b6b01f5f12d5e0b8aa56d
Jan Binge <github@binge.de>
2026-08-10 15:57:34 +0200

    fix tf_pd_knz_706: mon_id=verarbeitungsmonat, pd_auftr_id hinzufügen, Normalisierung als LEFT JOIN

--- dbt/models/fact/tf_pd_knz_706.sql
+++ dbt/models/fact/tf_pd_knz_706.sql
@@ -4,16 +4,22 @@
 
 SELECT
   1 AS Anzahl,
+  fc."pd_auftr_id",
   fc."org_id",
-  COALESCE(fc."pd_tae_beauf", 9999) AS "pd_tae_beauf",
-  COALESCE(fc."pd_veranl_stl", 99999) AS "pd_veranl_stl",
-  CAST(LEFT(
-    CAST(CAST(TO_CHAR(fc."pd_abschl_dat", 'YYYYMMDD') AS VARCHAR(8)) AS LONG VARCHAR),
-    6
-  ) AS INT) AS "mon_id",
-  COALESCE(fc."pd_abschl_art", 99999) AS "pd_abschl_art",
-  COALESCE(fc."pd_abschl_grund", 0) AS "pd_abschl_grund"
+  CASE WHEN ref_tkb."tkb_id" IS NULL THEN 9999 ELSE fc."pd_tae_beauf" END AS "pd_tae_beauf",
+  CASE WHEN ref_stl."ste_id" IS NULL THEN 99999 ELSE fc."pd_veranl_stl" END AS "pd_veranl_stl",
+  {{ var('verarbeitungsmonat') }} AS "mon_id",
+  CASE WHEN ref_asa."asa_id" IS NULL THEN 99999 ELSE fc."pd_abschl_art" END AS "pd_abschl_art",
+  CASE WHEN ref_asg."asg_id" IS NULL THEN 0 ELSE fc."pd_abschl_grund" END AS "pd_abschl_grund"
 FROM {{ ref('tf_pd_fc') }} AS fc
+LEFT JOIN sqlserver__bps__dbo__con_pd_knz_{{ var('verarbeitungsmonat') }}.vd_pd_taetigkeit_beauftragt ref_tkb
+  ON fc."pd_tae_beauf" = ref_tkb."tkb_id"
+LEFT JOIN sqlserver__bps__dbo__con_pd_knz_{{ var('verarbeitungsmonat') }}.vd_bps_stelle ref_stl
+  ON fc."pd_veranl_stl" = ref_stl."ste_id"
+LEFT JOIN sqlserver__bps__dbo__con_pd_knz_{{ var('verarbeitungsmonat') }}.vd_pd_abschlussart ref_asa
+  ON fc."pd_abschl_art" = ref_asa."asa_id"
+LEFT JOIN sqlserver__bps__dbo__con_pd_knz_{{ var('verarbeitungsmonat') }}.vd_bps_Abschlussgrund ref_asg
+  ON fc."pd_abschl_grund" = ref_asg."asg_id"
 WHERE fc."pd_fehl_typ" = 0
   AND CAST(LEFT(
     CAST(CAST(TO_CHAR(fc."pd_abschl_dat", 'YYYYMMDD') AS VARCHAR(8)) AS LONG VARCHAR),
